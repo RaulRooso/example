2c211044342d48562a9eb348a38f659363be396d
Raul Rooso <rooso.raul@gmail.com>
2025-07-01 18:54:40 +0300

    Login and registration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessinonController;
 
 
 
@@ -13,3 +15,10 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessinonController::class, 'create']);
+Route::post('/login', [SessinonController::class, 'store']);
